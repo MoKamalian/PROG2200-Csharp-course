dde8cbd7998cadac2dd9fe42bc9e103452883c31
Mo Kamalian <am445669@dal.ca>
2023-01-19 14:31:10 -0400

    lab1: module1-section 1 and 2 added

---
+++ lab1/PROG2200-Lab1-amir_kamalian/PROG2200-Lab1-amir_kamalian/Section1.cs
@@ -0,0 +1,52 @@
+﻿
+/*
+ * author: amir kamalian
+ * date:   jan 19 2023
+ * 
+ * 
+ */
+
+
+/*
+Challenge Questions
+
+- What is your name?
+- How old are you?
+- What month were you born in?
+~~additional questions~~
+- What is your marital status?
+- How many children do you have? 
+ 
+ */ 
+
+
+namespace Lab1
+{
+
+    class Section1 {
+        public static void Main(String[] args)
+        {
+            /* reading in general survey input */ 
+            Console.WriteLine("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you? ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("What month were you born in? ");
+            var birthMonth = Console.ReadLine();
+
+            Console.WriteLine("How many children do you have? ");
+            int numBirths = Int32.Parse(Console.ReadLine());
+
+            /* Outputting information given */
+            Console.WriteLine($"\n------\nName: {name}\nAge: {age}\nMonth Born: {birthMonth}\nNumber of Births: {numBirths}");
+
+            
+
+        }
+    }
+
+}
+
+
---
+++ lab1/PROG2200-Lab1-amir_kamalian/PROG2200-Lab1-amir_kamalian/Section2.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * 
+ * author: amir kamalian
+ * date:   jan 19 2023
+ * 
+ * 
+ */ 
+
+/* passcode challenge */ 
+
+using System;
+
+namespace Lab1 
+{
+    public class Section2
+    {
+
+
+        public static void Main(String[] args)
+        {
+
+            string passcode = "Password123";
+
+            Console.Write("Enter the super duper secret passcode: ");
+            var input = Console.ReadLine(); 
+
+            /* passcode verification and change */ 
+            if(input == passcode)
+            {
+                Console.WriteLine("Congrats! You now have access to NATO nuclear triad launch code! :)");
+
+                /* if passcode dosnt change, simply exit the program */ 
+                Console.WriteLine("Would you like to change the passcode [1 = yes; 2 = no]? ");
+                int choice = Int32.Parse(Console.ReadLine());
+                if(choice == 1)
+                {
+                    Console.WriteLine("Please enter new passcode: ");
+                    passcode = Console.ReadLine();
+                    Console.WriteLine("Passcode has been changed.  Goodbye :)");
+
+                }
+
+
+            } else {
+
+                Console.WriteLine("No nuclear launch codes for you :(");
+
+            } 
+
+        }
+
+
+    }
+}
+
